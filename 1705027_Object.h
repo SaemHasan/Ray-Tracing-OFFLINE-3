2f9bd6db4bbdecfc95ed0894e1f50fb9ae36ebd3
SaemHasan <1705027@ugrad.cse.buet.ac.bd>
2022-08-18 22:34:05 +0600

    general quadric done

--- 1705027_Object.h
+++ 1705027_Object.h
@@ -492,9 +492,96 @@ public:
         double a, b, c;
         double t_pos, t_neg;
         
+        Point dir = ray.rd;
+        dir.normalize();
+
+        Point origin = ray.r0;
+
+        a = A*dir.x*dir.x + B*dir.y*dir.y + C*dir.z*dir.z + D*dir.x*dir.y + E*dir.y*dir.z + F*dir.x*dir.z;
+        b = 2.0*(A*origin.x*dir.x + B*origin.y*dir.y + C*origin.z*dir.z + D*origin.x*dir.y + D*origin.y*dir.x + E*origin.y*dir.z + E*origin.z*dir.y + F*origin.x*dir.z + F*origin.z*dir.x + G*dir.x + H*dir.y + I*dir.z);
+        c = A*origin.x*origin.x + B*origin.y*origin.y + C*origin.z*origin.z + D*origin.x*origin.y + E*origin.y*origin.z + F*origin.x*origin.z + G*origin.x + H*origin.y + I*origin.z + J;
+
+        if(a == 0.0){
+            if(b!=0.0)
+                t_neg = (-c)/b;
+            else
+                t_neg = INF;
+            t_pos = INF;
+        }
+        else{
+            double d_sq = (b*b - 4.0*a*c); // b^2 - 4ac
+            if(d_sq<0.0){
+                t_neg = INF;
+                t_pos = INF;
+            }
+            else{
+                if(d_sq>0.0){
+                    // cout<<"d_sq: "<<d_sq<<endl;
+                    t_pos = (-b+sqrt(d_sq))/(2.0*a);
+                    t_neg = (-b-sqrt(d_sq))/(2.0*a);
+                    if(t_neg<0.0){
+                        t_neg = t_pos;
+                    }
+                }
+                else{
+                    t_neg = - b / (2.0*a);
+                    t_pos = INF;
+                }
+            }
+        }
+
+        // clip general quadric surface
+        Point intersect_point = ray.r0 + ray.rd * t_neg;
+        intersect_point = intersect_point - reference_point;
+        double x = intersect_point.x;
+        double y = intersect_point.y;
+        double z = intersect_point.z;
+
+        if(abs(x)>length || abs(y)>width || abs(z)>height){
+            t_neg = INF;
+        }
+
+        if(t_neg == INF){
+            intersect_point = ray.r0 + ray.rd * t_pos;
+            intersect_point = intersect_point - reference_point;
+            x = intersect_point.x;
+            y = intersect_point.y;
+            z = intersect_point.z;
+
+            if(abs(x)>length || abs(y)>width || abs(z)>height){
+                t_pos = INF;
+            }
+            else{
+                t_neg = t_pos;
+            }
+        }
+
+        // return for level 0
+        if (level == 0)
+        {
+            return t_neg;
+        }
+
+        // calculate normal
+        Point normal = Point(2.0*A*x + D*y + F*z + G, 2.0*B*y + D*x + E*z + H, 2.0*C*z + E*y + F*x + I);
+        normal.normalize();
+
+        // calculate color
+        Color intersectionPointColor = this->color;
+
+        calcLightPart(ray, color, level, t_neg, normal, intersect_point, intersectionPointColor);
         
+        // light part done
+        
+        // recursive reflection starts
+        
+        if(level >= levelsOfRecursion)
+        {
+            return t_neg;
+        }
+        recursiveReflection(ray, color, level, t_neg, normal, intersect_point);
 
-
+        return t_neg;
     }
 
     friend istream& operator>>(istream &in, General &g){
@@ -503,6 +590,9 @@ public:
         in >> g.length >> g.width >> g.height;
         in>>g.color;
         in>>g.coefficients[0]>>g.coefficients[1]>>g.coefficients[2]>>g.coefficients[3]>>g.shine;
+        if(g.length==0.0) g.length = INF;
+        if(g.width==0.0) g.width = INF;
+        if(g.height==0.0) g.height = INF;
         return in;
     }
 
